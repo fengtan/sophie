87b8c6eb24c623ae15ad2628ee03920dde962076
Fengtan <Fengtan@847318.no-reply.drupal.org>
2015-02-07 18:17:14 -0500

    Add table display.

---
+++ src/com/github/fengtan/solrgui/Dashboard.java
@@ -0,0 +1,47 @@
+package com.github.fengtan.solrgui;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+
+public class Dashboard {
+
+	public void paint() {
+	    Display display = new Display();
+	    Shell shell = new Shell(display);
+	    Table table = new Table(shell, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION);
+	    table.setLinesVisible(true);
+	    table.setHeaderVisible(true);
+	    String[] titles = { " ", "C", "!", "Description", "Resource", "In Folder", "Location" };
+	    for (int i = 0; i < titles.length; i++) {
+	      TableColumn column = new TableColumn(table, SWT.NONE);
+	      column.setText(titles[i]);
+	    }
+	    int count = 128;
+	    for (int i = 0; i < count; i++) {
+	      TableItem item = new TableItem(table, SWT.NONE);
+	      item.setText(0, "x");
+	      item.setText(1, "y");
+	      item.setText(2, "!");
+	      item.setText(3, "this stuff behaves the way I expect");
+	      item.setText(4, "almost everywhere");
+	      item.setText(5, "some.folder");
+	      item.setText(6, "line " + i + " in nowhere");
+	    }
+	    for (int i = 0; i < titles.length; i++) {
+	      table.getColumn(i).pack();
+	    }
+	    table.setSize(table.computeSize(SWT.DEFAULT, 200));
+	    shell.pack();
+	    shell.open();
+	    while (!shell.isDisposed()) {
+	      if (!display.readAndDispatch())
+	        display.sleep();
+	    }
+	    display.dispose();
+	}
+	
+}
--- src/com/github/fengtan/solrgui/Example.java
+++ src/com/github/fengtan/solrgui/Example.java
@@ -1,5 +1,8 @@
 package com.github.fengtan.solrgui;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
@@ -7,26 +10,24 @@ import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.RowLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Spinner;
-import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 
 public class Example {
 
-	public static void main(String[] args) {
+	
+	
+	public static void main(String[] args) throws MalformedURLException {
 
-		SolrServer solr = new HttpSolrServer("http://localhost:8983/solr/collection1");
+		URL server = new URL("http", "localhost", 8983, "/solr/collection1");
+		
+		SolrServer solr = new HttpSolrServer(server.toExternalForm());
 		
 		SolrQuery parameters = new SolrQuery();
-		parameters.set("q", "my query string");
+		parameters.set("q", "*:*");
 
 		try {
 			QueryResponse response = solr.query(parameters);
@@ -37,91 +38,9 @@ public class Example {
 			e.printStackTrace();
 		}
 		
+		Dashboard db = new Dashboard();
+		db.paint();
 		
-		/*
-		Display display = new Display();
-	    Shell shell = new Shell(display);
-
-	    // create a new GridLayout with two columns
-	    // of different size
-	    GridLayout layout = new GridLayout(2, false);
-
-	    // set the layout to the shell
-	    shell.setLayout(layout);
-	    
-	    // create a label and a button
-	    Label label = new Label(shell, SWT.NONE);
-	    label.setText("A label");
-	    Button button = new Button(shell, SWT.PUSH);
-	    button.setText("Press Me");
-
-	    // create a new label that will span two columns
-	    label = new Label(shell, SWT.BORDER);
-	    label.setText("This is a label");
-	    // create new layout data
-	    GridData data = new GridData(SWT.FILL, SWT.TOP, true, false, 2, 1);
-	    label.setLayoutData(data);
-
-	    // create a new label which is used as a separator
-	    label = new Label(shell, SWT.SEPARATOR | SWT.HORIZONTAL);
-	    
-	    // create new layout data
-	    data = new GridData(SWT.FILL, SWT.TOP, true, false);
-	    data.horizontalSpan = 2;
-	    label.setLayoutData(data);
-
-	    // create a right aligned button
-	    Button b = new Button(shell, SWT.PUSH);
-	    b.setText("New Button");
-
-	    data = new GridData(SWT.LEFT, SWT.TOP, false, false, 2, 1);
-	    b.setLayoutData(data);
-	    
-	     // create a spinner with min value 0 and max value 1000
-	    Spinner spinner = new Spinner(shell, SWT.READ_ONLY);
-	    spinner.setMinimum(0);
-	    spinner.setMaximum(1000);
-	    spinner.setSelection(500);
-	    spinner.setIncrement(1);
-	    spinner.setPageIncrement(100);
-	    GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, false);
-	    gridData.widthHint = SWT.DEFAULT;
-	    gridData.heightHint = SWT.DEFAULT;
-	    gridData.horizontalSpan = 2;
-	    spinner.setLayoutData(gridData);
-
-	    Composite composite = new Composite(shell, SWT.BORDER);
-	    gridData = new GridData(SWT.FILL, SWT.FILL, true, false);
-	    gridData.horizontalSpan = 2;
-	    composite.setLayoutData(gridData);
-	    composite.setLayout(new GridLayout(1, false));
-
-	    Text txtTest = new Text(composite, SWT.NONE);
-	    txtTest.setText("Testing");
-	    gridData = new GridData(SWT.FILL, SWT.FILL, true, false);
-	    txtTest.setLayoutData(gridData);
-
-	    Text txtMoreTests = new Text(composite, SWT.NONE);
-	    txtMoreTests.setText("Another test");
-	    
-	    Group group = new Group(shell, SWT.NONE);
-	    group.setText("This is my group");
-	    gridData = new GridData(SWT.FILL, SWT.FILL, true, false);
-	    gridData.horizontalSpan = 2;
-	    group.setLayoutData(gridData);
-	    group.setLayout(new RowLayout(SWT.VERTICAL));
-	    Text txtAnotherTest = new Text(group, SWT.NONE);
-	    txtAnotherTest.setText("Another test");
-
-	    shell.pack();
-	    shell.open();
-	    while (!shell.isDisposed()) {
-	      if (!display.readAndDispatch()) {
-	        display.sleep();
-	      }
-	    }
-	    display.dispose();
-*/	    
 	}
 	
 }
