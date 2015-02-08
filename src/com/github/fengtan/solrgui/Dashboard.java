a9d3556ff49a566fef5e948220bda8dee588f043
Fengtan <Fengtan@847318.no-reply.drupal.org>
2015-02-07 19:33:37 -0500

    Make columns dynamic.

--- src/com/github/fengtan/solrgui/Dashboard.java
+++ src/com/github/fengtan/solrgui/Dashboard.java
@@ -1,5 +1,9 @@
 package com.github.fengtan.solrgui;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 import org.eclipse.swt.SWT;
@@ -18,39 +22,20 @@ public class Dashboard {
 	    Table table = new Table(shell, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION); // TODO
 	    table.setLinesVisible(true);
 	    table.setHeaderVisible(true);
-	    String[] titles = { 
-	    "id",
-	    "index_id",
-	    "item_id",
-	    "hash",
-	    "site",
-	    "is_author",
-	    "tm_body$value",
-	    "spell",
-	    "ds_changed",
-	    "is_comment_count",
-	    "ds_created",
-	    "bs_promote",
-	    "ss_search_api_language",
-	    "bs_sticky",
-	    "tm_title",
-	    "ss_type",
-	    "_version_",
-	    "timestamp"};
 	    	    
-	    for (String title: titles) {
-	      TableColumn column = new TableColumn(table, SWT.NONE);
-	      column.setText(title);
-	    }
-
+	    List<String> titles = new ArrayList<String>(); // TODO hashed ? use of indexOf  
 	    for (SolrDocument document:list) {
-	      TableItem item = new TableItem(table, SWT.NONE);
-	      int count = 0;
-	      for (Object value: document.values()) {
-	    	  item.setText(count++, value.toString());
-	      }
+	    	TableItem item = new TableItem(table, SWT.NONE);
+	    	for (Map.Entry<String, Object> field:document.entrySet()) { // TODO is there a utility function to do that quikly
+	    		if (!titles.contains(field.getKey())) {
+	  		      	new TableColumn(table, SWT.NONE).setText(field.getKey());
+	    			titles.add(field.getKey());
+	    		}
+	    		item.setText(titles.indexOf(field.getKey()), field.getValue().toString());
+	    	}
 	    }
-	    for (int i = 0; i < titles.length; i++) {
+	    
+	    for (int i = 0; i < titles.size(); i++) {
 	      table.getColumn(i).pack();
 	    }
 	    table.setSize(table.computeSize(SWT.DEFAULT, 200));
