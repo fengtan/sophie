35fe5932ae396cc77b9276b532f13bd7d8a8ec6c
Fengtan <Fengtan@847318.no-reply.drupal.org>
2015-03-08 22:19:20 -0400

    Add color provider.

--- src/com/github/fengtan/solrgui/viewers/SolrGUILabelProvider.java
+++ src/com/github/fengtan/solrgui/viewers/SolrGUILabelProvider.java
@@ -2,24 +2,30 @@ package com.github.fengtan.solrgui.viewers;
 
 import java.util.Objects;
 
+import org.eclipse.jface.viewers.IColorProvider;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
 
 import com.github.fengtan.solrgui.beans.SolrGUIDocument;
 import com.github.fengtan.solrgui.beans.SolrGUIServer;
 
-public class SolrGUILabelProvider extends LabelProvider implements ITableLabelProvider {
+public class SolrGUILabelProvider extends LabelProvider implements ITableLabelProvider, IColorProvider {
 
+	private static final Color GREEN = Display.getCurrent().getSystemColor(SWT.COLOR_GREEN);
+	private static final Color RED = Display.getCurrent().getSystemColor(SWT.COLOR_RED);
+	private static final Color YELLOW = Display.getCurrent().getSystemColor(SWT.COLOR_YELLOW);
+	
 	private String[] fields;
 	
 	public SolrGUILabelProvider(SolrGUIServer server) {
 		this.fields = server.getFields();
 	}
 	
-	/**
-	 * @see org.eclipse.jface.viewers.ITableLabelProvider#getColumnText(java.lang.Object, int)
-	 */
+	@Override
 	public String getColumnText(Object element, int columnIndex) {
 		SolrGUIDocument document = (SolrGUIDocument) element;
 		switch (columnIndex) {
@@ -46,10 +52,28 @@ public class SolrGUILabelProvider extends LabelProvider implements ITableLabelPr
 		}
 	}
 	
-	/**
-	 * @see org.eclipse.jface.viewers.ITableLabelProvider#getColumnImage(java.lang.Object, int)
-	 */
+	@Override
 	public Image getColumnImage(Object element, int columnIndex) {
+		return null;
+	}
+
+	@Override
+	public Color getBackground(Object element) {
+		SolrGUIDocument document = (SolrGUIDocument) element;
+		switch (document.getChange()) {
+			case ADDED:
+				return GREEN;
+			case DELETED:
+				return RED;
+			case UPDATED:
+				return YELLOW;
+			default:
+				return null;
+		}
+	}
+
+	@Override
+	public Color getForeground(Object element) {
 		return null;
 	}
 
