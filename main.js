4db5c2d7a009900c87c867ff3a4f6b9acd0da62c
Fengtan 冯坦 <Fengtan@847318.no-reply.drupal.org>
2016-07-24 19:56:21 -0400

    Refactor.

--- main.js
+++ main.js
@@ -10,13 +10,14 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow();
+  mainWindow.maximize();
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/app/index.html`)
 
   // Open the DevTools.
-  //mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
